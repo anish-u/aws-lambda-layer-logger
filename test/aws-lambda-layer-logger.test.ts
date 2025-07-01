f938496fd35c0fdf751457a1d872b63ef47ee8c7
anish ummenthala <ummenthala.anish@gmail.com>
2025-07-01 00:51:01 -0700

    Initial commit

---
+++ test/aws-lambda-layer-logger.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsLambdaLayerLogger from '../lib/aws-lambda-layer-logger-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-lambda-layer-logger-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsLambdaLayerLogger.AwsLambdaLayerLoggerStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
